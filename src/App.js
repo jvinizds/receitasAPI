3b9876c55e4c05d7ef8b21760f614dfd191a9f44
João Vinicius <joaoviniciuszds@gmail.com>
2021-04-09 09:20:53 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { FaCocktail } from 'react-icons/fa'
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+
 
 function App() {
+
+  const [procuraTipo, setProcuraTipo] = useState('')
+  const [pesquisa, setPesquisa] = useState('')
+  const [prato, setPrato] = useState(null)
+
+
+  async function buscaCoquetel() {
+    let url = `www.themealdb.com/api/json/v1/1/random.php`
+
+    await fetch(url)
+    .then(response =>response.json())
+    .then(data => {
+      setPrato(data)
+    })
+    .catch(function (error) {
+      console.error('Houve um erro na requisição' + error.message)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+      <Navbar bg="secondary">
+        <Navbar.Brand href="#inicio" className="text-light"  >CocktailGuide <FaCocktail className="text-dark" size="40" /></Navbar.Brand>
+      </Navbar>
+
+      <Form>
+        <Form.Row className="justify-content-center mt-4"> 
+          <Col xs="auto">
+            <Form.Control as="select" id="escolha" onChange={e => setProcuraTipo(e.target.value)}>
+              <option value="meal" >Random Meal</option>
+              <option value="cocktail" >Random Cocktail</option>
+            </Form.Control>
+          </Col>
+          &nbsp;
+          <Col xs="auto">
+            <Button variant="warning" onClick={}>Search</Button>
+          </Col>
+        </Form.Row>
+      </Form>
+    </>
   );
 }
 
